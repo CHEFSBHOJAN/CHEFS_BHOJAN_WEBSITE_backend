e57e55a989d9d436db4bab27fdbed3727f5425cc
nagesh-merva <officialnagesh.merva@gmail.com>
2024-07-09 22:52:19 +0530

    modified

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def margao_orders():
             })
             total_amount += item_total
             order['items_list'] = items_list
-        order['total_amount'] = round(total_amount / 1.2, 2)
+            order['total_amount'] = round(total_amount / 1.2, 2)
         
     return render_template('margao_orders.html', orders=orders)
 
