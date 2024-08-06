bcc8bda4ca1dc795807ca23f32dfd7f67eb6086c
CHEFSBHOJAN <bhojanchefs@gmail.com>
2024-08-06 12:50:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -426,5 +426,53 @@ def Update_Ponda_Status():
         return jsonify({'status': 'success', 'message': 'Margao marked as Off'}), 200
     return jsonify({'status': 'error', 'message': 'Invalid request'}), 400
 
+# digital menu backend
+
+@app.route('/api/getmenumargao', methods=['GET'])
+def MargaoMenu():
+    if request.method == 'OPTIONS':
+        return jsonify({'status': 'success', 'message': 'CORS preflight request handled successfully'}), 200
+    
+    try:
+        menu_file_path = os.path.join(os.path.dirname(__file__), 'margaomenu.json')
+        with open(menu_file_path, 'r') as file:
+            dishes = json.load(file)
+        return jsonify({'dishes': dishes}), 200
+    except FileNotFoundError:
+        error_message = "menu.json file not found"
+        print(error_message)
+        return jsonify({'error': error_message}), 500
+    except json.JSONDecodeError:
+        error_message = "Error decoding JSON from menu.json"
+        print(error_message)
+        return jsonify({'error': error_message}), 500
+    except Exception as e:
+        error_message = f"An unexpected error occurred: {str(e)}"
+        print(error_message)
+        return jsonify({'error': error_message}), 500
+    
+@app.route('/api/getmenuponda', methods=['GET'])
+def PondaMenu():
+    if request.method == 'OPTIONS':
+        return jsonify({'status': 'success', 'message': 'CORS preflight request handled successfully'}), 200
+    
+    try:
+        menu_file_path = os.path.join(os.path.dirname(__file__), 'pondamenu.json')
+        with open(menu_file_path, 'r') as file:
+            dishes = json.load(file)
+        return jsonify({'dishes': dishes}), 200
+    except FileNotFoundError:
+        error_message = "menu.json file not found"
+        print(error_message)
+        return jsonify({'error': error_message}), 500
+    except json.JSONDecodeError:
+        error_message = "Error decoding JSON from menu.json"
+        print(error_message)
+        return jsonify({'error': error_message}), 500
+    except Exception as e:
+        error_message = f"An unexpected error occurred: {str(e)}"
+        print(error_message)
+        return jsonify({'error': error_message}), 500
+
 if __name__ == "__main__":
     socketio.run(app, debug=True, host='0.0.0.0', port=8080)
